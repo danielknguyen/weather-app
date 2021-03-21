1f0c8b83bec03c3bc44294204f0fc7c269501980
Phuong Nguyen <phuongnguyen@Phuongs-MacBook-Pro.local>
2021-03-21 15:40:19 -0700

    added fahreinheit, % to temp and humidity, in addition included a weather icon that is specific to the weather description

--- weather-app/src/components/DisplayWeather.js
+++ weather-app/src/components/DisplayWeather.js
@@ -2,24 +2,25 @@ import React from 'react';
 import '../css/DisplayWeather.css';
 
 const DisplayWeather = ({weatherData}) => {
-
-    console.log('weather data in display weather: ',weatherData);
-
+    // console.log('weather data in display weather: ',weatherData);
     return (
         <div className="displayWeatherContainer">
             <div className="displayWeatherOuterWrapper">
                 <div className="displayWeatherInner"> 
-                    <div className="weatherIconOuter">
-                        <span>Weather: {weatherData.weather}</span>
+                    <div className="icon">
+                        <img src={"http://openweathermap.org/img/wn/" + weatherData.icon + ".png"} alt={weatherData.weather}/>
                     </div>
-                    <div className="temperature">
-                        <span>Temp: {weatherData.temperature}</span>
+                    <div className="description weatherData">
+                        <span><b>Weather:</b> {weatherData.weather}</span>
                     </div>
-                    <div className="humidity">
-                        <span>Humidity: {weatherData.humidity}</span>
+                    <div className="temperature weatherData">
+                        <span><b>Temp:</b> {weatherData.temperature +" °F"}</span>
                     </div>
-                    <div className="wind">
-                        <span>Wind:{(weatherData.wind ? weatherData.wind + " mph" : "")}</span>
+                    <div className="humidity weatherData">
+                        <span><b>Humidity:</b> {weatherData.humidity + "%"}</span>
+                    </div>
+                    <div className="wind weatherData">
+                        <span><b>Wind:</b> {(weatherData.wind ? weatherData.wind + " mph" : "")}</span>
                     </div>
                 </div>
             </div>
